9b5f90e474939dfc64c5e91a1437e0c14a7896b0
nicktu12 <nickteets@gmail.com>
2018-11-08 15:59:53 -0700

    Locate issue body on response

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = app => {
   app.log('Yay, the app was loaded!')
 
   app.on('issues.opened', async context => {
+    const body = context.payload.issue.body;
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     return context.github.issues.createComment(issueComment)
   })
