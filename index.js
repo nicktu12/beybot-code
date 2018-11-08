ef4896760b0f715b37f36c078408a2684a25dd25
nicktu12 <nickteets@gmail.com>
2018-11-08 16:27:42 -0700

    Update bot response

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ module.exports = app => {
   app.log('Yay, the app was loaded!')
 
   app.on('issues.opened', async context => {
+    const triggerWords = ["Bey", "Beyonce", "Beyoncé", "Yonce", "Yoncé", "Mrs. Carter", "Mrs Carter", "JayZ", "Jay-Z", "Jay Z", "Sasha Fierce", "Destiny's Child", "Destinys Child", "Solange", "Knowles"];
     const body = context.payload.issue.body;
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    const issueComment = context.issue({ body: 'Queen Bey reigns' })
     return context.github.issues.createComment(issueComment)
   })
 
