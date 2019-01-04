6075b244ae93d5a252a6a79c4b8f70e22fdd803c
nicktu12 <nickteets@gmail.com>
2019-01-04 10:24:09 -0700

    Add semicolons

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = app => {
   // Your code here
   const fetch = require('node-fetch');
-  app.log('Yay, the app was loaded!')
+  app.log('Yay, the app was loaded!');
 
   const triggerWords = ["Bey", "Beyonce", "Beyoncé", "Yonce", "Yoncé", "Mrs. Carter", "Mrs Carter", "JayZ", "Jay-Z", "Jay Z", "Sasha Fierce", "Destiny's Child", "Destinys Child", "Solange", "Knowles"];
 
@@ -10,7 +10,7 @@ module.exports = app => {
       .then(response => response.json())
       .then(json => json.data.id)
       .catch(error => { app.log(error) });
-  }
+  };
 
   app.on('issues.opened', async context => {
     const issueBody = context.payload.issue.body;
@@ -20,25 +20,25 @@ module.exports = app => {
     const issue = issueBody + ' ' + issueTitle;
 
     const containsArray = triggerWords.map(word => 
-      issue.toLowerCase().includes(word.toLowerCase())
+      issue.toLowerCase().includes(word.toLowerCase());
     );
 
     if (containsArray.includes(true)) {
       const bey = await getBeyGif();
-      console.log(bey)
-      const issueComment = context.issue({ body: `![](https://media.giphy.com/media/${bey}/giphy.gif)` })
-      return context.github.issues.createComment(issueComment)
-    }
+      console.log(bey);
+      const issueComment = context.issue({ body: `![](https://media.giphy.com/media/${bey}/giphy.gif)` });
+      return context.github.issues.createComment(issueComment);
+    };
 
-  })
+  });
 
   app.on('pull_request.opened', async context => {
-    app.log('suuuuuh;')
-  })
+    app.log(context);
+  });
 
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-}
+};
